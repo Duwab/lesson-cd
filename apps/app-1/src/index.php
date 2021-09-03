66885359958906390318dbd79fd9a3d5f1f4bcc1
Antoine Dubois <antoine.dubois@commandersact.com>
2021-09-03 16:01:29 +0200

    Configure CI for app-2

---
+++ apps/app-1/src/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$readJSONFile = file_get_contents('../config.json');
+$array = json_decode($readJSONFile, TRUE);
+$version = $array['version'];
+
+?>
+
+<h1>Hello World</h1>
+<p>
+    This is <code>app-1</code> version: <code><?php echo $version; ?></code>
+</p>
